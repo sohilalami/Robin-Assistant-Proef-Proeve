ed7f3664389e09d2a752976fcf42c2cda1d3217f
Damien Hensen <damhen80@gmail.com>
2021-03-16 16:30:45 +0100

    Style update Login Client select

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-var login = document.getElementById('log-in');
-login.addEventListener('click', function() {
-    location.href = "./client-select.html";
-});
+var login = document.getElementById("log-in");
+var edit = document.getElementById("profile-edit");
+var pageTitle = document.getElementById("page-title");
+var clients = document.getElementsByClassName("client");
+
+if (login) {
+	login.addEventListener("click", function (e) {
+		e.preventDefault();
+		location.href = "./client-select.html";
+	});
+}
+
+if (edit) {
+	edit.addEventListener("click", function () {
+		if (this.classList.contains("blue")) {
+			this.classList.remove("blue");
+			this.textContent = "PROFIELEN BEHEREN";
+			pageTitle.textContent = "Kies een cliënt";
+		} else {
+			this.classList.add("blue");
+			this.textContent = "KLAAR";
+			pageTitle.textContent = "Profielen beheren";
+		}
+	});
+}
