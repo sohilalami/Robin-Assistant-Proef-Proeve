6ffdffac47a908cf4dfc99d0984d77c7d5b63632
Damien Hensen <damhen80@gmail.com>
2021-03-17 12:46:46 +0100

    Edit pagina navigatie

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var edit = document.getElementById("profile-edit");
 var pageTitle = document.getElementById("page-title");
 var clients = document.getElementsByClassName("client");
 var selectInputs = document.getElementsByClassName("select-input");
-
+var editButtons = document.querySelectorAll(".profile-edit .buttons .btn-2");
 
 if (login) {
 	login.addEventListener("click", function (e) {
@@ -14,6 +14,16 @@ if (login) {
 
 if (edit) {
 	edit.addEventListener("click", function () {
+		for (let i = 0; i < clients.length; i++) {
+			const editOverlay = clients[i].querySelector(".edit-overlay");
+
+			if (editOverlay.classList.contains("hidden")) {
+				editOverlay.classList.remove("hidden");
+			} else {
+				editOverlay.classList.add("hidden");
+			}
+		}
+
 		if (this.classList.contains("blue")) {
 			this.classList.remove("blue");
 			this.textContent = "PROFIELEN BEHEREN";
@@ -27,12 +37,23 @@ if (edit) {
 }
 
 if (selectInputs) {
-    for (let i = 0; i < selectInputs.length; i++) {
-        const selectWrapper = selectInputs[i].querySelector('.select'),
-              select        = selectWrapper.querySelector('select'),
-              style         = window.getComputedStyle(select),
-              width         = style.getPropertyValue('width');
+	for (let i = 0; i < selectInputs.length; i++) {
+		const selectWrapper = selectInputs[i].querySelector(".select"),
+			select = selectWrapper.querySelector("select"),
+			style = window.getComputedStyle(select),
+			width = style.getPropertyValue("width");
 
-        selectWrapper.style.width = width;
-    }
+		selectWrapper.style.width = width;
+	}
+}
+
+if (editButtons) {
+	for (let i = 0; i < editButtons.length; i++) {
+		const editButton = editButtons[i];
+
+		editButton.addEventListener("click", function (e) {
+			e.preventDefault();
+			location.href = "./client-select.html";
+		});
+	}
 }
